a0cc1eb6a83cf8b4b372f0950f0815a495597cb4
ThuyNT13 <ngo.thuy38@gmail.com>
2017-11-11 21:04:46 -0800

    Remove test spec files

--- tests/SpecTestSpec.js
+++
@@ -1,7 +0,0 @@
-describe('testHello()', function() {
-  describe('returns a hello', function() {
-    it('Hello World!', function() {
-      expect(testHello()).toEqual('Hello World!');
-    });
-  });
-})
